a25d9cc1969984f832996d888d87f1837ff04fa5
Egor Kaeta <kaeta.egor2015@yandex.ru>
2022-11-16 22:49:17 +0300

    fix: fix bugs in multi audios playing

--- sound-quiz/src/scripts/custom-audio-player.js
+++ sound-quiz/src/scripts/custom-audio-player.js
@@ -151,7 +151,6 @@ export function createAudioPlayer(audioSrc) {
   volumeSlider.addEventListener("input", changeVolume);
 
   function play() {
-    audio.play();
     audio.addEventListener("timeupdate", changePlayerSlider);
     if (document.querySelector(".play-pause-block__pause-button")) {
       let playingBefore = document.querySelector(
@@ -164,10 +163,9 @@ export function createAudioPlayer(audioSrc) {
     playButton.addEventListener("click", pause);
     let allAudios = document.querySelectorAll("audio");
     allAudios.forEach((element) => {
-      if (element.src != audioSrc) {
-        element.pause();
-      }
+      element.pause();
     });
+    audio.play();
   }
 
   function pause() {
